4a369215af163647fccaefbbac39a0b043445dd1
Valdevino Romao <valdevino.romao@gmail.com>
2021-01-26 20:47:47 -0300

    feito os metodos de lista

--- index.php
+++ index.php
@@ -7,13 +7,27 @@ $sql = new Sql();
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 //echo json_encode($usuarios);
+//carrega um usuario
+//$root = new Usuario();
 
-$root = new Usuario();
+//$root->loadbyId(9);
 
-$root->loadbyId(9);
+//echo $root;
 
-echo $root;
+//carrega uma lista de usuario
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("us");
+
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","123456");
+
+echo $usuario;
 
 
  ?>
